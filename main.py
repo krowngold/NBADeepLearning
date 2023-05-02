51b8f6433a8a3346654717d85619d4086bc68fa6
krowngold <noah.krohngold@gmail.com>
2023-05-01 17:25:29 -0700

    Starter files for testing

---
+++ main.py
@@ -0,0 +1,14 @@
+from basketball_reference_web_scraper import client
+from basketball_reference_web_scraper.data import OutputType
+
+# client.player_box_scores(
+#     day=1, month=1, year=2017, 
+#     output_type=OutputType.CSV, 
+#     output_file_path="./1_1_2017_box_scores.csv"
+# )
+
+
+# get box scores for each day as a json
+# sort csv by team
+# create aggregates by adding values across days and averaging for entire season
+# 
